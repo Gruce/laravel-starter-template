1c7438f8f7e8213fb3b87e1431650749668cab9c
Hassan Kadhim <66673458+HassanKadhim@users.noreply.github.com>
2022-06-18 21:54:41 +0300

    add

---
+++ NPM_WATCH.bat
@@ -0,0 +1,11 @@
+:: Name:     Laravel App
+:: Purpose:  Npm Run Watch Application
+:: Author:   hassan57905@gmail.com && credit: gruceing@gmail.com
+:: Revision: June 2022 - initial version
+
+@echo off
+
+ECHO Starting Server...
+
+npm run watch
+pause
